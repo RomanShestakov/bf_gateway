899df9b61eaef65143451a8c42a6299c65d59447
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-04-21 18:24:11 +0100

    change rest api

--- src/bf_gateway_getActiveEventTypes.erl
+++
@@ -1,9 +0,0 @@
--module(bf_gateway_getActiveEventTypes).
-
--export([init/1, to_html/2, generate_etag/2]).
-
--include_lib("webmachine/include/webmachine.hrl").
-
-init([]) -> {ok, undefined}.
-to_html(ReqData, Context) -> {bf_gateway:getActiveEventTypes(), ReqData, Context}.
-generate_etag(ReqData, Context) -> {wrq:raw_path(ReqData), ReqData, Context}.
