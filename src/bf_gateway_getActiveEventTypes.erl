39191be77365a1ef75134cf33195a40981f14c66
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-04-21 09:09:57 +0100

    add getActiveEventTypes

--- src/bf_gateway_getActiveEventTypes.erl
+++ src/bf_gateway_getActiveEventTypes.erl
@@ -1,9 +1,9 @@
--module(bf_rest).
+-module(bf_gateway_getActiveEventTypes).
 
 -export([init/1, to_html/2, generate_etag/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
-to_html(ReqData, Context) -> {"Hello, new world2", ReqData, Context}.
+to_html(ReqData, Context) -> {bf_gateway:getActiveEventTypes(), ReqData, Context}.
 generate_etag(ReqData, Context) -> {wrq:raw_path(ReqData), ReqData, Context}.
