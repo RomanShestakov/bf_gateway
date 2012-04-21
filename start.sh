ca9052c5adfc367d1c88d34b17a7ba07c923bb63
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-04-21 18:55:40 +0100

    add config for start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name bf_gateway@127.0.0.1 \
-    -setcookie rs -s bf_gateway_app start
+    -setcookie rs -s bf_gateway_app start -config ebin/bf_gateway
